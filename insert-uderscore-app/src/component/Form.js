96ec0b7d87acd9b9f10fedd0a3a70439baea019b
skosana721 <wonganiskosana@gmail.com>
2021-04-29 11:31:56 +0200

    created a form component with input with type of number and added a button to form with type of submit

---
+++ insert-uderscore-app/src/component/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <form action="">
+        <input type="number" />
+        <button type="submit"></button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;

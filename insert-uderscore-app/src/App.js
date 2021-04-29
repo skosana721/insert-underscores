cd86e5bb0128ec5c5750c24ed80f3cb6322973b1
skosana721 <wonganiskosana@gmail.com>
2021-04-29 11:37:14 +0200

    rendered the form to the app component

--- insert-uderscore-app/src/App.js
+++ insert-uderscore-app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Form from "./component/Form";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <Form />
     </div>
   );
 }
